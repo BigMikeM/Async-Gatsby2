76c6929651ab0159c531e2214a1b38a19d84e5e1
BigMikeM <65140398+BigMikeM@users.noreply.github.com>
2020-09-17 16:26:49 -0400

    Deploying to Heroku...maybe

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,18 @@
 import React from "react"
 import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
-    <Header headerText='WHAT UP, PLANET!' />
+      <Link to="/contact/">Hit me up!</Link>
+      <Header headerText="WHAT UP, PLANET!" />
       <p>Strange place</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+      <img
+        src="https://source.unsplash.com/random/1600x900"
+        className="header-image"
+        alt=""
+      />
     </div>
-  );
+  )
 }
